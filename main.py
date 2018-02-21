26faad79864cf5a250cf9bf1d2da7e3f06d38254
MrValdez <shunyvaldez@gmail.com>
2018-02-21 09:08:05 +0800

    added code to update board

--- main.py
+++ main.py
@@ -11,7 +11,37 @@ def display_board():
             print(cell,end="|")
         print("")
 
+def ask_for_move(symbol):
+    coordinates = input("""Where do you want to put {} (use "x, y" format)? """.format(symbol))
 
+    try:
+        coordinates = coordinates.split(",")
+        x, y = coordinates
+        x = int(x.strip())
+        y = int(y.strip())
+        
+        if 3 > x <= 0:
+            raise ValueError
+
+    except ValueError:
+        print(""" Invalid input.
+ Please use "x, y" format. Top-left coordinate is 1, 1
+ Example: 2, 2
+""")
+        return ask_for_move(symbol)
+
+    return x, y
+
+def play_move(symbol, x, y):
+    # move coordinates to zero start
+    x -= 1
+    y -= 1
+    board[(y * 3) + x] = symbol
 
 reset()
+display_board()
+
+symbol_p1 = "X"
+x, y = ask_for_move(symbol_p1)
+play_move(symbol_p1, x, y)
 display_board()
